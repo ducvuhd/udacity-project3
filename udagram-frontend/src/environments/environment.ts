01244d3ead92e519c47f7d78347196c6fff7470c
Vu Nguyen <ducvutk.01@gmail.com>
2023-10-03 03:56:30 +0700

    update url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2aedc244e27547318a34b5eff630a5c-2048358129.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
