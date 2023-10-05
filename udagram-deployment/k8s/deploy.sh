da7c909c48247bd343ae4bdd8a9fb7686ed1a6f0
Vu Nguyen <Vu.Nguyen4@coxautoinc.com>
2023-10-05 13:42:29 +0700

    update node  option and build image version

--- udagram-deployment/k8s/deploy.sh
+++ udagram-deployment/k8s/deploy.sh
@@ -1,24 +1,9 @@
-kubectl delete deployments udagram-api-feed
-kubectl delete deployments udagram-api-user
-kubectl delete deployments udagram-api-frontend
-kubectl delete deployments reverse-proxy
-
-kubectl delete services udagram-api-feed
-kubectl delete services udagram-api-user
-kubectl delete services udagram-api-frontend
-kubectl delete services reverse-proxy
-
-kubectl apply -f aws-secret.yaml
-kubectl apply -f env-secret.yaml
-kubectl apply -f env-configmap.yaml
-
 kubectl apply -f backend-feed-deployment.yaml
-kubectl apply -f backend-feed-service.yaml
 kubectl apply -f backend-user-deployment.yaml
-kubectl apply -f backend-user-service.yaml
-
 kubectl apply -f frontend-deployment.yaml
-kubectl apply -f frontend-service.yaml
-
 kubectl apply -f reverseproxy-deployment.yaml
+
+kubectl apply -f backend-feed-service.yaml
+kubectl apply -f backend-user-service.yaml
+kubectl apply -f frontend-service.yaml
 kubectl apply -f reverseproxy-service.yaml
